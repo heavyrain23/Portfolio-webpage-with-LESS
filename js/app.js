75362999a13c2112343a61f23f3d0ac295ba3c89
Alex <failemotions95@gmail.com>
2019-10-27 23:07:53 +0200

    modal animation done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    // Filter
+
     let filter = $("[data-filter]");
 
     filter.on('click', function(event) {
@@ -24,4 +26,66 @@ $(function() {
           });
         }
     });
+
+
+// Modal
+
+const modalCall = $("[data-modal]");
+const modalClose = $("[data-close]");
+
+modalCall.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalId = $this.data('modal');
+
+    $(modalId).addClass('show');
+    $("body").addClass('no-scroll');
+
+    setTimeout(function () {
+        $(modalId).find(".modal__dialog").css({
+            transform: "rotate(0)"
+        });
+    }, 200);
+});
+
+
+modalClose.on("click", function (event) {
+    event.preventDefault();
+
+    let $this = $(this);
+    let modalParent = $this.parents('.modal');
+
+    modalParent.find(".modal_dialog").css({
+        transform: "rotateX(90deg)"
+    });
+
+    setTimeout(function () {
+        modalParent.removeClass('show');
+        $("body").removeClass('no-scroll');
+    }, 200);
+});
+
+$(".modal").on('click', function (event) {
+    let $this = $(this);
+
+    $this.find(".modal_dialog").css({
+        transform: "rotateX(90deg)"
+    });
+
+    setTimeout(function () {
+        $this.removeClass('show');
+        $("body").removeClass('no-scroll');
+    }, 200);
+
+});
+
+
+
+$('.modal__dialog').on("click", function (event) {
+event.stopPropagation();
+
+});
+
+
 });
